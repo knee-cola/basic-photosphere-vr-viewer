201e64e390dea2d191daed17bb11b1c997807e1c
Nikola Derežić <nikola.derezic@gmail.com>
2019-01-23 11:19:14 +0100

    created a more advanced WakeLock polyfill

---
+++ src/components/WakeLockPolyfill.js
@@ -0,0 +1,30 @@
+import * as NoSleep from 'nosleep.js';
+
+/**
+ * Stops smartphone from going to sleep
+ */
+export class WakeLockPolyfill {
+    enable() {
+        if ('getWakeLock' in navigator) {
+            navigator.getWakeLock('screen').then((wakeLockObj) => {
+                this.wakeLockObj = wakeLockObj;
+                this.wakeLockRequest = wakeLockObj.createRequest();
+            }).catch((err) => {
+              return console.error('Could not obtain wake lock', err);
+            });
+        } else {
+            this.noSleep = new NoSleep();
+            this.noSleep.enable();
+        }
+    }
+
+    disable() {
+        if(this.wakeLockObj) {
+            this.wakeLockObj.cancel();
+            this.wakeLockObj = null;
+        } else {
+            this.noSleep.disable();
+            this.noSleep = null;
+        }
+    }
+}
