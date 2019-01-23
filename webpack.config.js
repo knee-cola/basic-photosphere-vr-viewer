0c078377753454f222978124027f3c93b64ce105
knee-cola <nikola.derezic@gmail.com>
2019-01-23 20:37:09 +0100

    webpack config change: dev server is serving over HTTPS (WakeLock API requirenment)

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   devServer: {
     contentBase: './docs',
     host: '0.0.0.0', // listen on all IP addresses
-    port: 8080
+    port: 8080,
+    https: true // needed for the WakeLock API to work (in Chrome)
   },
 };
